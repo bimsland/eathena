bimsland/eathena

commit 3f5e16e0aefcd2da9d03b1b98913d8499e56ee8f

- Modified party_send_movemap and guild_send_info_short to send the mini-dots to the player logging on. - Added functions clif_party/guild_xy_single to send the xy dots to a single fd.
git-svn-id: http://svn.eathena.ws/svn/ea/trunk@5359 8d3bc1bb-15e8-0310-adf8-f98a08d51996

src/map/clif.c
@@ -2972,17 +2972,33 @@ int clif_guildstorageequiplist(struct map_session_data *sd,struct guild_storage 
 // Guild XY locators [Valaris]
 int clif_guild_xy(struct map_session_data *sd)
 {
-unsigned char buf[10];
+	unsigned char buf[10];
 
-nullpo_retr(0, sd);
+	nullpo_retr(0, sd);
 
-WBUFW(buf,0)=0x1eb;
-WBUFL(buf,2)=sd->status.account_id;
-WBUFW(buf,6)=sd->bl.x;
-WBUFW(buf,8)=sd->bl.y;
-clif_send(buf,packet_len_table[0x1eb],&sd->bl,GUILD_SAMEMAP_WOS);
+	WBUFW(buf,0)=0x1eb;
+	WBUFL(buf,2)=sd->status.account_id;
+	WBUFW(buf,6)=sd->bl.x;
+	WBUFW(buf,8)=sd->bl.y;
+	clif_send(buf,packet_len_table[0x1eb],&sd->bl,GUILD_SAMEMAP_WOS);
 
-return 0;
+	return 0;
+}
+
+/*==========================================
+ * Sends x/y dot to a single fd. [Skotlex]
+ *------------------------------------------
+ */
+
+int clif_guild_xy_single(int fd, struct map_session_data *sd)
+{
+	WFIFOHEAD(fd,packet_len_table[0x1eb]);
+	WFIFOW(fd,0)=0x1eb;
+	WFIFOL(fd,2)=sd->status.account_id;
+	WFIFOW(fd,6)=sd->bl.x;
+	WFIFOW(fd,8)=sd->bl.y;
+	WFIFOSET(fd,packet_len_table[0x1eb]);
+	return 0;
 }
 
 // Guild XY locators [Valaris]
@@ -6670,6 +6686,24 @@ int clif_party_xy(struct map_session_data *sd)
 	
 	return 0;
 }
+
+/*==========================================
+ * Sends x/y dot to a single fd. [Skotlex]
+ *------------------------------------------
+ */
+
+int clif_party_xy_single(int fd, struct map_session_data *sd)
+{
+	WFIFOHEAD(fd,packet_len_table[0x107]);
+	WFIFOW(fd,0)=0x107;
+	WFIFOL(fd,2)=sd->status.account_id;
+	WFIFOW(fd,6)=sd->bl.x;
+	WFIFOW(fd,8)=sd->bl.y;
+	WFIFOSET(fd,packet_len_table[0x107]);
+	return 0;
+}
+
+
 /*==========================================
  * パーティHP通知
  *------------------------------------------
@@ -8702,11 +8736,6 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 	}
 	// param all
 	clif_initialstatus(sd);
-	// party
-	party_send_movemap(sd);
-	// guild
-	guild_send_memberinfoshort(sd,1);
-
 	if(battle_config.pc_invincible_time > 0) {
 		if(map_flag_gvg(sd->bl.m))
 			pc_setinvincibletimer(sd,battle_config.pc_invincible_time<<1);
@@ -8716,6 +8745,11 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 
 	map_addblock(&sd->bl);	// ブロック登録
 	clif_spawnpc(sd);	// spawn
+
+	// party
+	party_send_movemap(sd);
+	// guild
+	guild_send_memberinfoshort(sd,1);
 
 	// weight max , now
 	clif_updatestatus(sd,SP_MAXWEIGHT);

src/map/guild.c
@@ -880,7 +880,8 @@ int guild_member_leaved(int guild_id,int account_id,int char_id,int flag,
 int guild_send_memberinfoshort(struct map_session_data *sd,int online)
 {
 	struct guild *g;
-
+	int  i;
+	
 	nullpo_retr(0, sd);
 
 	if(sd->status.guild_id<=0)
@@ -893,10 +894,18 @@ int guild_send_memberinfoshort(struct map_session_data *sd,int online)
 		sd->status.account_id,sd->status.char_id,online,sd->status.base_level,sd->status.class_);
 
 	if( !online ){	// ログアウトするならsdをクリアして終了
-		int i=guild_getindex(g,sd->status.account_id,sd->status.char_id);
+		i=guild_getindex(g,sd->status.account_id,sd->status.char_id);
 		if(i>=0)
 			g->member[i].sd=NULL;
 		return 0;
+	} else if (sd->fd) {
+		//Send XY dot updates. [Skotlex]
+		for(i=0; i < MAX_GUILD; i++) {
+			if (!g->member[i].sd || g->member[i].sd == sd ||
+				g->member[i].sd->bl.m != sd->bl.m)
+				continue;
+			clif_guild_xy_single(sd->fd, g->member[i].sd);
+		}
 	}
 
 	if( sd->state.guild_sent!=0 )	// ギルド初期送信データは送信済み
@@ -906,14 +915,12 @@ int guild_send_memberinfoshort(struct map_session_data *sd,int online)
 	guild_check_conflict(sd);
 
 	// あるならギルド初期送信データ送信
-	if( (g=guild_search(sd->status.guild_id))!=NULL ){
-		guild_check_member(g);	// 所属を確認する
-		if(sd->status.guild_id==g->guild_id){
-			clif_guild_belonginfo(sd,g);
-			clif_guild_notice(sd,g);
-			sd->state.guild_sent=1;
-			sd->guild_emblem_id=g->emblem_id;
-		}
+	guild_check_member(g);	// 所属を確認する
+	if(sd->status.guild_id==g->guild_id){
+		clif_guild_belonginfo(sd,g);
+		clif_guild_notice(sd,g);
+		sd->state.guild_sent=1;
+		sd->guild_emblem_id=g->emblem_id;
 	}
 	return 0;
 }

src/map/party.c
@@ -470,6 +470,7 @@ int party_recv_movemap(int party_id,int account_id,int char_id, unsigned short m
 // パーティメンバの移動
 int party_send_movemap(struct map_session_data *sd)
 {
+	int i;
 	struct party *p;
 
 	nullpo_retr(0, sd);
@@ -478,6 +479,19 @@ int party_send_movemap(struct map_session_data *sd)
 		return 0;
 	intif_party_changemap(sd,1);
 
+	
+	p=party_search(sd->status.party_id);
+	if (p && sd->fd) {
+		//Send dots of other party members to this char. [Skotlex]
+		for(i=0; i < MAX_PARTY; i++) {
+			if (!p->member[i].sd	|| p->member[i].sd == sd ||
+				p->member[i].sd->bl.m != sd->bl.m)
+				continue;
+			clif_party_xy_single(sd->fd, p->member[i].sd);
+		}
+		
+	}
+	
 	if( sd->state.party_sent )	// もうパーティデータは送信済み
 		return 0;
 
@@ -485,7 +499,7 @@ int party_send_movemap(struct map_session_data *sd)
 	party_check_conflict(sd);
 	
 	// あるならパーティ情報送信
-	if( (p=party_search(sd->status.party_id))!=NULL ){
+	if(p){
 		party_check_member(p);	// 所属を確認する
 		if(sd->status.party_id==p->party_id){
 			clif_party_main_info(p,sd->fd);

src/map/clif.h
@@ -254,6 +254,7 @@ int clif_party_leaved(struct party *p,struct map_session_data *sd,int account_id
 int clif_party_message(struct party *p,int account_id,char *mes,int len);
 int clif_party_move(struct party *p,struct map_session_data *sd,int online);
 int clif_party_xy(struct map_session_data *sd);
+int clif_party_xy_single(int fd, struct map_session_data *sd);
 int clif_party_hp(struct map_session_data *sd);
 int clif_hpmeter(struct map_session_data *sd);
 
@@ -282,6 +283,7 @@ int clif_guild_delalliance(struct map_session_data *sd,int guild_id,int flag);
 int clif_guild_oppositionack(struct map_session_data *sd,int flag);
 int clif_guild_broken(struct map_session_data *sd,int flag);
 int clif_guild_xy(struct map_session_data *sd);
+int clif_guild_xy_single(int fd, struct map_session_data *sd);
 int clif_guild_xy_remove(struct map_session_data *sd);
 
 

commit 196b4276ea82cc42999922ed78f543df7762fcd5

- Fixed guild emblem not showing when you log in (caused by r5359) - Fixed server not sending party/guild info when you enter a map - Fixed server not removing member minimap dot when you leave a guild
git-svn-id: http://svn.eathena.ws/svn/ea/trunk@10624 8d3bc1bb-15e8-0310-adf8-f98a08d51996

src/map/clif.c
@@ -8012,15 +8012,6 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 	clif_updatestatus(sd,SP_MAXWEIGHT);
 	clif_updatestatus(sd,SP_WEIGHT);
 
-	if(battle_config.pc_invincible_time > 0) {
-		if(map_flag_gvg(sd->bl.m))
-			pc_setinvincibletimer(sd,battle_config.pc_invincible_time<<1);
-		else
-			pc_setinvincibletimer(sd,battle_config.pc_invincible_time);
-	}
-	map_addblock(&sd->bl);	// ƒuƒƒbƒN“o˜^
-	clif_spawn(&sd->bl);	// spawn
-
 	// Party
 	if(sd->status.party_id) {
 		party_send_movemap(sd);
@@ -8030,6 +8021,15 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 	// guild
 	if(sd->status.guild_id)
 		guild_send_memberinfoshort(sd,1);
+
+	if(battle_config.pc_invincible_time > 0) {
+		if(map_flag_gvg(sd->bl.m))
+			pc_setinvincibletimer(sd,battle_config.pc_invincible_time<<1);
+		else
+			pc_setinvincibletimer(sd,battle_config.pc_invincible_time);
+	}
+	map_addblock(&sd->bl);	// ƒuƒƒbƒN“o˜^
+	clif_spawn(&sd->bl);	// spawn
 
 	if(map[sd->bl.m].flag.pvp) {
 		if(!battle_config.pk_mode) { // remove pvp stuff for pk_mode [Valaris]

src/map/guild.c
@@ -715,10 +715,10 @@ int guild_reply_invite(struct map_session_data *sd,int guild_id,int flag)
 			return 0;
 		}
 
-
 		//interI‚Ö’Ç‰Á—v‹
 		guild_makemember(&m,sd);
 		intif_guild_addmember( sd->guild_invite, &m );
+		//TODO: send a minimap update to this player
 		return 0;
 	}else{		// ‹‘”Û
 		sd->guild_invite=0;
@@ -756,8 +756,10 @@ int guild_member_added(int guild_id,int account_id,int char_id,int flag)
 	}
 
 		// ¬Œ÷
-	sd->state.guild_sent=0;
-	sd->status.guild_id=guild_id;
+	sd->state.guild_sent = 0;
+	sd->status.guild_id = g->guild_id;
+	sd->guild_emblem_id = g->emblem_id;
+	//TODO: send new emblem info to others
 
 	if( sd2!=NULL )
 		clif_guild_inviteack(sd2,2);
@@ -770,8 +772,7 @@ int guild_member_added(int guild_id,int account_id,int char_id,int flag)
 }
 
 // ƒMƒ‹ƒh’E‘Ş—v‹
-int guild_leave(struct map_session_data *sd,int guild_id,
-	int account_id,int char_id,const char *mes)
+int guild_leave(struct map_session_data* sd, int guild_id, int account_id, int char_id, const char* mes)
 {
 	struct guild *g;
 
@@ -790,9 +791,9 @@ int guild_leave(struct map_session_data *sd,int guild_id,
 	intif_guild_leave(sd->status.guild_id, sd->status.account_id, sd->status.char_id,0,mes);
 	return 0;
 }
+
 // ƒMƒ‹ƒh’Ç•ú—v‹
-int guild_expulsion(struct map_session_data *sd,int guild_id,
-	int account_id,int char_id,const char *mes)
+int guild_expulsion(struct map_session_data* sd, int guild_id, int account_id, int char_id, const char* mes)
 {
 	struct map_session_data *tsd;
 	struct guild *g;
@@ -829,47 +830,51 @@ int guild_expulsion(struct map_session_data *sd,int guild_id,
 	return 0;
 }
 
-int guild_member_leaved(int guild_id,int account_id,int char_id,int flag,
-	const char *name,const char *mes) // rewrote [LuzZza]
+int guild_member_leaved(int guild_id, int account_id, int char_id, int flag, const char* name, const char* mes)
 {
 	int i;
-	struct guild *g = guild_search(guild_id);
-	struct map_session_data *sd = map_charid2sd(char_id);
-	struct map_session_data *online_member_sd;
+	struct guild* g = guild_search(guild_id);
+	struct map_session_data* sd = map_charid2sd(char_id);
+	struct map_session_data* online_member_sd;
 
 	if(g == NULL)
-		return 0;
+		return 0; // no such guild (error!)
 	
-	for(i=0;i<g->max_member;i++) {
-		if( g->member[i].account_id == account_id &&
-			g->member[i].char_id == char_id ){
+	for(i = 0; i < g->max_member; i++)
+		if( g->member[i].account_id == account_id && g->member[i].char_id == char_id )
+			break;
 
-				if((online_member_sd = guild_getavailablesd(g)) == NULL)
-					return 0;
+	if (i == g->max_member)
+		return 0; // not a member (inconsistency!)
 
-				if(!flag)
-					clif_guild_leave(online_member_sd, name, mes);
-				else
-					clif_guild_expulsion(online_member_sd, name, mes, account_id);
+	online_member_sd = guild_getavailablesd(g);
+	if(online_member_sd == NULL)
+		return 0; // noone online to inform
 
-				memset(&g->member[i],0,sizeof(struct guild_member));
-				clif_guild_memberlist(online_member_sd);
+	if(!flag)
+		clif_guild_leave(online_member_sd, name, mes);
+	else
+		clif_guild_expulsion(online_member_sd, name, mes, account_id);
 
-				if(sd != NULL && sd->status.guild_id == guild_id) {
-					if (sd->state.storage_flag == 2) //Close the guild storage.
-						storage_guild_storageclose(sd);
-					sd->status.guild_id=0;
-					sd->guild_emblem_id=0;
-					sd->state.guild_sent=0;
-					
-					guild_send_dot_remove(sd);
-					clif_charnameupdate(sd); //Update display name [Skotlex]
-				}
-				return 0;
-		}
-					
+	// remove member from guild
+	memset(&g->member[i],0,sizeof(struct guild_member));
+	clif_guild_memberlist(online_member_sd);
+
+	// update char, if online
+	if(sd != NULL && sd->status.guild_id == guild_id)
+	{
+		// do stuff that needs the guild_id first, BEFORE we wipe it
+		if (sd->state.storage_flag == 2) //Close the guild storage.
+			storage_guild_storageclose(sd);
+		guild_send_dot_remove(sd);
+
+		sd->status.guild_id = 0;
+		sd->guild_emblem_id = 0;
+		sd->state.guild_sent = 0;
+		
+		clif_charnameupdate(sd); //Update display name [Skotlex]
+		//TODO: send emblem update to self and people around
 	}
-
 	return 0;
 }
 
@@ -975,6 +980,7 @@ int guild_recv_memberinfoshort(int guild_id,int account_id,int char_id,int onlin
 			continue;
 
 		clif_guild_xy_single(g->member[idx].sd->fd, g->member[i].sd);
+		clif_guild_xy_single(g->member[i].sd->fd, g->member[idx].sd);
 	}			
 
 	return 0;

src/map/party.c
@@ -427,6 +427,7 @@ int party_member_leaved(int party_id,int account_id,int char_id)
 		sd->status.party_id=0;
 		sd->state.party_sent=0;
 		clif_charnameupdate(sd); //Update name display [Skotlex]
+		//TODO: hp bars should be cleared too
 	}
 	return 0;
 }
@@ -532,12 +533,15 @@ void party_send_movemap(struct map_session_data *sd)
 		}
 	}
 
-	if (sd->fd) { //Send dots of other party members to this char. [Skotlex]
+	if (sd->fd) { // synchronize minimap positions with the rest of the party
 		for(i=0; i < MAX_PARTY; i++) {
-			if (p->data[i].sd &&
+			if (p->data[i].sd && 
 				p->data[i].sd != sd &&
 				p->data[i].sd->bl.m == sd->bl.m)
+			{
 				clif_party_xy_single(sd->fd, p->data[i].sd);
+				clif_party_xy_single(p->data[i].sd->fd, sd);
+			}
 		}
 	}
 	return;